b8edd59ffcdb5d26fdfb32fc1538d8466010eac1
Pinku rathour <pinkurathour2016@gmail.com>
2024-09-10 17:16:02 +0530

    add new Projects

---
+++ src/main.js/gsap.js
@@ -0,0 +1,14 @@
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".about-section",
+    scrub: true,
+    start: "bottom 34%",
+    end: "top 2%",
+    pin: true,
+    markers: true,
+  },
+});
+
+const tl2 =gsap.timeline({
+  
+})
